ae8aec8073cb2612b07d78377933dc8baf6b78f1
Irish Micole Cando <irishmicolecando@gmail.com>
2021-11-01 10:31:32 +0800

    Defined a function to ask the number of apples bought and compute the price

---
+++ AssignmentNo2_2_V2.py
@@ -0,0 +1,12 @@
+# Create a program that will ask how many apples and oranges you want to buy.
+# Display the total amount you need to pay if apple price is 20 pesos and orange is 25.
+# Display the output in the following format.
+# The total amount is ______.
+
+def ask_apples_number_to_compute_price():
+    apple_number = int(input("How many apples do you want to buy? "))
+    apple_price = 20 * apple_number
+    return apple_price
+
+
+apple = ask_apples_number_to_compute_price()
