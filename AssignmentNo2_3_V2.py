11376e3d6377f512ce0116543b1c7da65149034e
Irish Micole Cando <irishmicolecando@gmail.com>
2021-11-01 11:47:08 +0800

    Defined a function and variable to ask the amount of money of the user

---
+++ AssignmentNo2_3_V2.py
@@ -0,0 +1,10 @@
+# Create a program which you will enter the amount of money you have, it will also ask for the price of an apple.
+# Display the maximum number of apples that you can buy and the remaining money that you will have.
+# Display the output in the following format.
+# You can buy ___ apples and your change is ___ pesos.
+
+def ask_money():
+    _money = float(input("How much money do you have? "))
+    return _money
+
+money = ask_money()
